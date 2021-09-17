6ddc4e5ffd896bcf935a53d9652d895f8bfc091d
Tevvin Strong <tevvindstrong@gmail.com>
2021-09-16 22:57:39 -0400

    Header component rendering and styled after figma design

--- meme-generator/src/App.vue
+++ meme-generator/src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="memeGenerator">
+    <MemeGeneratorHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MemeGeneratorHeader from './components/Header/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MemeGeneratorHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #memeGenerator {
+    height: 963px;
+    display: flex;
+    justify-content: center;
+  }
 </style>
