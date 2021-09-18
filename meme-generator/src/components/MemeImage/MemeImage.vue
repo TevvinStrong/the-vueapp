eec72c89882d93960c26dd3211e6d49e0bfd86ce
Tevvin Strong <tevvindstrong@gmail.com>
2021-09-17 23:24:58 -0400

    Created new file MemeImage, added simple image and styling to memeImage component

---
+++ meme-generator/src/components/MemeImage/MemeImage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="meme-image">
+        <img src="https://cdn.kqed.org/wp-content/uploads/sites/38/2019/03/tomemeornot3-768x384.jpg" alt="Funny Picture/Funny meme">
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .meme-image {
+        width: 700px;
+        height: 362px;
+        border: 1px solid black;
+    }
+</style>
