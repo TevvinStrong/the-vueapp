7869055be4cdbe4aed5d5996d1407b1adeaa195e
Tevvin Strong <tevvindstrong@gmail.com>
2021-09-17 23:26:26 -0400

    Imported MemeGeneratorImage component

--- meme-generator/src/App.vue
+++ meme-generator/src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="memeGenerator">
     <MemeGeneratorHeader />
+    <MemeGeneratorImage />
   </div>
 </template>
 
 <script>
 import MemeGeneratorHeader from './components/Header/Header.vue';
+import MemeGeneratorImage from './components/MemeImage/MemeImage.vue';
 
 export default {
   name: 'App',
   components: {
-    MemeGeneratorHeader
+    MemeGeneratorHeader,
+    MemeGeneratorImage
   }
 }
 </script>
 
 <style>
   #memeGenerator {
+    margin: auto;
+    width: 800px;
     height: 963px;
     display: flex;
-    justify-content: center;
+    justify-content: flex-start;
+    flex-direction: column;
   }
 </style>
