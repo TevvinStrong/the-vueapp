0a53f06b44a9f0498a3b9f51710e0ee28caefc0d
Tevvin Strong <tevvindstrong@gmail.com>
2021-09-19 00:20:56 -0400

    Making get request via fetch to meme api, able to log the data return from get request

---
+++ meme-generator/src/components/MemeImage/MemeImage.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="meme-image">
+        <!-- <img src="https://cdn.kqed.org/wp-content/uploads/sites/38/2019/03/tomemeornot3-768x384.jpg" alt="Funny Picture/Funny meme"> -->
+        <!-- <img :v-bind="image" alt="Image retuned from meme api"> -->
+        <!-- <img :v-for="Images" alt="Images from API"> -->
+        <li v-for="image in images" :key="image.id">
+            {{ image.url }}
+        </li>
+    </div>
+</template>
+
+<script>
+    export default {
+  name: "MemeImage",
+  data() {
+    return {
+      images: {}
+    }
+  },
+  beforeMount(){
+    this.getImages();
+  },
+  methods: {
+    async getImages(){
+      const res = await fetch('https://api.imgflip.com/get_memes');
+      const image = await res.json();
+      this.image = image.data.memes;
+      console.log('>>>>>>>>>>', this.image);
+    }
+  }
+};
+</script>
+
+<style scoped>
+    .meme-image {
+        width: 700px;
+        height: 362px;
+        border: 1px solid black;
+    }
+</style>
