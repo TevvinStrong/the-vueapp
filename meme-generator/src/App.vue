be7ac4812acf1214f9f2cc18cdd4d14544f8fe33
Michael Jarvis <mjarvis1997@gmail.com>
2022-02-02 21:48:08 -0500

    MG-007 | Page routing

--- meme-generator/src/App.vue
+++ meme-generator/src/App.vue
@@ -1,22 +1,34 @@
 <template>
-  <div id="image-selection-page">
-    <ImageSelectionPage />
+  <div id="app">
+    <div id="nav">
+    <!-- Example of routing to the other page
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      -->
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-  import ImageSelectionPage from '@/pages/ImageSelectionPage';
-
-  export default {
-    name: 'App',
-    components: {
-      ImageSelectionPage
-    }
-  }
-</script>
-
 <style>
-  #image-selection-page {
-    height: 100%; 
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
---
+++ meme-generator/src/views/Edit.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="edit">
+    <h1>This is the page where memes will be edited</h1>
+  </div>
+</template>
