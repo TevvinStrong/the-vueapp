76b09bdd94a3d1253590c2add199dd56d8cee19f
Tevvin Strong <tevvindstrong@gmail.com>
2022-02-18 22:20:17 -0500

    1st commit Added new route and view to handle users custom meme

--- meme-generator/src/router/index.js
+++ meme-generator/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import AddImage from '../views/AddImage.vue'
 
 Vue.use(VueRouter)
 
@@ -18,6 +19,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Edit.vue'),
     props: true
+  },
+  {
+    path: '/add',
+    name: 'Add',
+    component: AddImage
   }
 ]
 
---
+++ meme-generator/src/views/AddImage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="add-image">
+      <div class="image-url">
+        <input class="addimage-url" type="url" placeholder="url">
+      </div>
+      <div class="custom-meme">
+        <input class="top-text" type="url" placeholder="Top-Text">
+        <img src="https://www.getbgd.com/wp-content/uploads/2017/05/placeholder-image.png" alt="placeholder image template">
+        <input class="bottom-text" type="url" placeholder="Bottom-Text">
+      </div>
+      <button class="submit">Generate</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AddImage',
+  props: {
+  },
+  components: {
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
